8cec1fec9716f6283d915910d8e694b8f4dbd6ea
aaajr97 <aaajr97@outlook.com>
2022-11-04 14:12:16 +0000

    Added hand and chips classes, adjusted for user to decide whether the value of an ace is 1 or 11, depending on the value of their hand

--- blackjack.py
+++ blackjack.py
@@ -5,8 +5,8 @@ GLOBAL VARIABLES USED BY ALL CLASSES
 """
 suits = ('Hearts', 'Clubs', 'Diamonds', 'Spades')
 ranks = ('Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King')
-values = {'Ace': 1, 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
-          'Jack': 10, 'Queen': 10, 'King': 10}
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
+          'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
 
 
 class Card:  # Card class
@@ -27,7 +27,8 @@ class Deck:  # Deck class that automatically generates a deck by creating a list
             for rank in ranks:
                 self.deck.append(Card(suit, rank))
 
-    def __str__(self):  # Allows user to see the composition of the deck by calling the print function of the Deck object
+    def __str__(
+            self):  # Allows user to see the composition of the deck by calling the print function of the Deck object
         deck_composition = ""
         for card in self.deck:
             deck_composition += '\n' + card.__str__()
@@ -38,3 +39,37 @@ class Deck:  # Deck class that automatically generates a deck by creating a list
 
     def deal(self):
         return self.deck.pop()
+
+
+class Hand:  # Allows user to see what cards they possess
+
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self, card):
+        self.cards.append(card)  # The card passed in is the card object obtained from the Deck().deal method
+        self.value += values[card.rank]  # Obtains the value of card object and adds it to the self.value attribute
+
+        if card.rank == 'Ace':  # Tracking aces
+            self.aces += 1
+
+    def adjust_for_aces(self):  # Method that adjusts for aces incase the value of the players hand exceeds 21.
+        # To compensate the values of the aces is reduced from 11 to 1.
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
+
+
+class Chips: # Chips class that keeps track of players starting chips, bets and winnings.
+
+    def __init__(self, total=100):
+        self.total = total
+        self.bet = 0
+
+    def win_bet(self):
+        self.total += self.bet
+
+    def lose_bet(self):
+        self.total -= self.bet
